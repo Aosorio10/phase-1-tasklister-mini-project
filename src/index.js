530eb1dc8cfa4fef11b700bade96556e9d0a8e97
Alejandro Osorio <avot3110@hotmail.com>
2023-05-08 11:10:00 -0400

    Tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const formElement = document.querySelector('#create-task-form');
+const listElement = document.querySelector('#tasks');
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputElement = document.querySelector('#new-task-description');
+  const description = inputElement.value;
+
+  if (description !== '') {
+    const listItemElement = document.createElement('li');
+    listItemElement.innerText = description;
+    listElement.appendChild(listItemElement);
+    inputElement.value = '';
+  }
 });
